b0772c5475bb1178b63b3667099f2b79c8b4dda1
jasanikuldip <jasanikuldip258@gmail.com>
2021-12-14 22:02:47 +0530

    upcoming services

--- HTML/js/index.js
+++ HTML/js/index.js
@@ -6,3 +6,24 @@ window.onload = function () {
 function hidePrivacy() {
     document.getElementById("privacy-policy").classList.add("d-none");
 }
+
+$(document).ready(function () {
+    $(window).breakpoints({
+      triggerOnInit: true,
+    });
+    $(window).bind("breakpoint-change", function (e) {
+      var $outputListening = $("#output-listening");
+      $outputListening.find("#output-listening--current").html(e.to);
+      $outputListening.find("#output-listening--previous").html(e.from);
+    });
+    $(window).on("lessThan-lg", function () {
+      $(".sec-part").removeClass("container");
+      $(".sec-part").addClass("container-fluid");
+      $("tr").addClass("card custom-card shadow");
+    });
+    $(window).on("greaterThan-lg", function () {
+      $(".sec-part").addClass("container");
+      $(".sec-part").removeClass("container-fluid");
+      $("tr").removeClass("card custom-card shadow");
+    });
+});
